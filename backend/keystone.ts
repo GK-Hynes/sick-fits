94cc8a7323932cc7aa6f9a900988570801745dd3
Gerard Hynes <gkhynes@gmail.com>
2021-09-02 16:29:15 +0100

    Add ProductImage schema

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -3,10 +3,12 @@ import { config, createSchema } from "@keystone-next/keystone/schema";
 import { createAuth } from "@keystone-next/auth";
 import { User } from "./schemas/User";
 import { Product } from "./schemas/Product";
+import { ProductImage } from "./schemas/ProductImage";
 import {
   withItemData,
   statelessSessions
 } from "@keystone-next/keystone/session";
+import { insertSeedData } from "./seed-data";
 
 const databaseURL =
   process.env.DATABASE_URL || "mongodb://localhost/keystone-sick-fits-tutorial";
@@ -36,12 +38,19 @@ export default withAuth(
     },
     db: {
       adapter: "mongoose",
-      url: databaseURL
+      url: databaseURL,
       // TODO: add data seeding here
+      async onConnect(keystone) {
+        console.log("Connected to the database!");
+        if (process.argv.includes("--seed-data")) {
+          await insertSeedData(keystone);
+        }
+      }
     },
     lists: createSchema({
       User,
-      Product
+      Product,
+      ProductImage
     }),
     ui: {
       // Show the UI only for people who pass this test
