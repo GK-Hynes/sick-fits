a5e1a662f4d591a044a616fcbdef13a50683d3d9
Gerard Hynes <gkhynes@gmail.com>
2021-09-06 20:53:36 +0100

    Add product using mutation

--- frontend/components/CreateProduct.js
+++ frontend/components/CreateProduct.js
@@ -1,23 +1,68 @@
-import React, { useState } from 'react'
-import useForm from '../lib/useForm'
-import Form from "./styles/Form"
+import React, { useState } from "react";
+import gql from "graphql-tag";
+import { useMutation } from "@apollo/client";
+import DisplayError from "./ErrorMessage";
+import useForm from "../lib/useForm";
+import Form from "./styles/Form";
+
+const CREATE_PRODUCT_MUTATION = gql`
+  mutation CREATE_PRODUCT_MUTATION(
+    # Which variables are passed in and what types are they?
+    $name: String!
+    $description: String!
+    $price: Int!
+    $image: Upload
+  ) {
+    createProduct(
+      data: {
+        name: $name
+        description: $description
+        price: $price
+        status: "AVAILABLE"
+        photo: { create: { image: $image, altText: $name } }
+      }
+    ) {
+      id
+      price
+      description
+      name
+    }
+  }
+`;
 
 export default function CreateProduct() {
-  const {inputs, handleChange, clearForm, resetForm} = useForm({
+  const { inputs, handleChange, clearForm, resetForm } = useForm({
     image: "",
     name: "Nice shoes",
     price: 3734,
     description: "These are the best shoes"
-  })
+  });
+  const [createProduct, { loading, error, data }] = useMutation(
+    CREATE_PRODUCT_MUTATION,
+    {
+      variables: inputs
+    }
+  );
   return (
-    <Form onSubmit={e => {
-      e.preventDefault()
-      console.log(inputs)
-    }}>
-      <fieldset>
+    <Form
+      onSubmit={async (e) => {
+        e.preventDefault();
+        // Submit input fields to backend
+        const res = await createProduct();
+        clearForm();
+      }}
+    >
+      <DisplayError error={error} />
+      <fieldset disabled={loading} aria-busy={loading}>
         <label htmlFor="image">
           Image
-          <input type="file" name="image" id="image" onChange={handleChange} required/>
+          <input
+            type="file"
+            name="image"
+            id="image"
+            onChange={handleChange}
+            required
+          />
         </label>
         <label htmlFor="name">
           Name
