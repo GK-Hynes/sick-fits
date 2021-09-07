d3bf20f0a25e7ef42f7af8ef55f3e09f4d5677a8
Gerard Hynes <gkhynes@gmail.com>
2021-09-07 21:05:15 +0100

    Add ability to update products

--- frontend/components/Product.js
+++ frontend/components/Product.js
@@ -1,20 +1,34 @@
-import React from 'react'
-import ItemStyles from "./styles/ItemStyles"
-import Title from "./styles/Title"
-import PriceTag from "./styles/PriceTag"
-import Link from "next/link"
-import formatMoney from "../lib/formatMoney"
+import React from "react";
+import ItemStyles from "./styles/ItemStyles";
+import Title from "./styles/Title";
+import PriceTag from "./styles/PriceTag";
+import Link from "next/link";
+import formatMoney from "../lib/formatMoney";
 
-export default function Product({product}) {
+export default function Product({ product }) {
   return (
-   <ItemStyles>
-     <img src={product?.photo?.image?.publicUrlTransformed} alt={product.name} />
-     <Title>
-       <Link href={`/product/${product.id}`}>{product.name}</Link>
-     </Title>
-     <PriceTag>{formatMoney(product.price)}</PriceTag>
-     <p>{product.description}</p>
-     {/* TODO: Add buttons to edit and delete item */}
-     </ItemStyles>
-  )
+    <ItemStyles>
+      <img
+        src={product?.photo?.image?.publicUrlTransformed}
+        alt={product.name}
+      />
+      <Title>
+        <Link href={`/product/${product.id}`}>{product.name}</Link>
+      </Title>
+      <PriceTag>{formatMoney(product.price)}</PriceTag>
+      <p>{product.description}</p>
+      <div className="buttonList">
+        <Link
+          href={{
+            pathname: "update",
+            query: {
+              id: product.id
+            }
+          }}
+        >
+          Edit ✏
+        </Link>
+      </div>
+    </ItemStyles>
+  );
 }
