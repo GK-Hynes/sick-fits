33edaacbeda58f6a1a56a31f35b178c878e0724c
Gerard Hynes <gkhynes@gmail.com>
2021-09-25 21:28:38 +0100

    Create sign up flow

--- frontend/components/SignUp.js
+++ frontend/components/SignUp.js
@@ -5,51 +5,61 @@ import { useMutation } from "@apollo/client";
 import { CURRENT_USER_QUERY } from "./User";
 import Error from "./ErrorMessage";
 
-const SIGNIN_MUTATION = gql`
-  mutation SIGNIN_MUTATION($email: String!, $password: String!) {
-    authenticateUserWithPassword(email: $email, password: $password) {
-      ... on UserAuthenticationWithPasswordSuccess {
-        item {
-          id
-          email
-          name
-        }
-      }
-      ... on UserAuthenticationWithPasswordFailure {
-        code
-        message
-      }
+const SIGNUP_MUTATION = gql`
+  mutation SIGNUP_MUTATION(
+    $email: String!
+    $name: String!
+    $password: String!
+  ) {
+    createUser(data: { email: $email, name: $name, password: $password }) {
+      id
+      email
+      name
     }
   }
 `;
 
-export default function SignIn() {
+export default function SignUp() {
   const { inputs, handleChange, resetForm } = useForm({
     email: "",
-    password: ""
+    password: "",
+    name: ""
   });
 
-  const [signin, { data, loading }] = useMutation(SIGNIN_MUTATION, {
-    variables: inputs,
+  const [signup, { data, loading, error }] = useMutation(SIGNUP_MUTATION, {
+    variables: inputs
     // refetch the currently logged in user
-    refetchQueries: [{ query: CURRENT_USER_QUERY }]
+    // refetchQueries: [{ query: CURRENT_USER_QUERY }]
   });
 
   async function handleSubmit(e) {
     e.preventDefault();
-    const res = await signin();
+    const res = await signup().catch(console.error);
     resetForm();
   }
-  const error =
-    data?.authenticateUserWithPassword.__typename ===
-    "UserAuthenticationWithPasswordFailure"
-      ? data?.authenticateUserWithPassword
-      : undefined;
+
   return (
     <Form method="POST" onSubmit={handleSubmit}>
-      <h2>Sign Into Your Account</h2>
+      <h2>Sign Up for an Account</h2>
       <Error error={error} />
       <fieldset>
+        {data?.createUser && (
+          <p>
+            Signed up with {data.createUser.email} - Please go ahead and sign
+            in!
+          </p>
+        )}
+        <label htmlFor="name">
+          Your Name
+          <input
+            type="text"
+            name="name"
+            placeholder="Your name"
+            autoComplete="name"
+            value={inputs.name}
+            onChange={handleChange}
+          />
+        </label>
         <label htmlFor="email">
           Email
           <input
@@ -72,7 +82,7 @@ export default function SignIn() {
             onChange={handleChange}
           />
         </label>
-        <button type="submit">Sign In!</button>
+        <button type="submit">Sign Up!</button>
       </fieldset>
     </Form>
   );
